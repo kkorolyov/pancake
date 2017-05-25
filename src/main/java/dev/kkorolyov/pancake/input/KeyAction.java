ce4e9534594f44689cb7fb0a8eb06ea6d84651c6
kkorolyov <kirillk753@gmail.com>
2017-05-24 23:42:43 -0700

    Cleanup

--- src/main/java/dev/kkorolyov/pancake/input/KeyAction.java
+++ src/main/java/dev/kkorolyov/pancake/input/KeyAction.java
@@ -4,6 +4,9 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import dev.kkorolyov.pancake.entity.control.Action;
+import dev.kkorolyov.simplelogs.Level;
+import dev.kkorolyov.simplelogs.Logger;
+import dev.kkorolyov.simplelogs.format.Formatters;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 
@@ -12,13 +15,13 @@ import javafx.scene.input.MouseButton;
  */
 public class KeyAction {
 	private static final float HOLD_THRESHOLD = .5f;
-//	private static final Logger log = Logger.getLogger(KeyAction.class.getName(), Level.DEBUG, new PrintWriter(System.err));	// TODO Temp
+	private static final Logger log = Logger.getLogger(Level.DEBUG, Formatters.simple());
 
 	private final Set<Enum<?>> keys = new LinkedHashSet<>();
 	private final Action onPress, onHold, onRelease;
 	private float holdTime = 0;
 	private KeyActionState state = KeyActionState.OPEN;
-	
+
 	/**
 	 * Constructs a new {@code KeyAction} tied to a set of keys and buttons.
 	 * @param onPress action returned on button press
@@ -31,15 +34,15 @@ public class KeyAction {
 		this.onPress = onPress;
 		this.onHold = onHold;
 		this.onRelease = onRelease;
-		
+
 		for (Enum<?> input : inputs) {
 			if (!(input instanceof KeyCode || input instanceof MouseButton))
 				throw new IllegalArgumentException("Not a valid key: " + input);
-			
+
 			keys.add(input);
 		}
 	}
-	
+
 	/**
 	 * Polls this {@code KeyAction} with all current active inputs.
 	 * @param inputs current active inputs
@@ -49,58 +52,55 @@ public class KeyAction {
 	public Action activate(Set<Enum<?>> inputs, float dt) {
 		return state.activate(this, inputs.containsAll(keys), dt);
 	}
-	
+
 	private static abstract class KeyActionState {
-		@SuppressWarnings("synthetic-access")
-		static final KeyActionState
-			OPEN = new KeyActionState() {
-				@Override
-				Action activate(KeyAction client, boolean allPressed, float dt) {
-					if (allPressed) {
-//						log.debug("Keys pressed: " + client.keys);
-						
-						client.holdTime = 0;
-						client.state = PRESSED;
-						return client.onPress;
+		static final KeyActionState OPEN = new KeyActionState() {
+			@Override
+			Action activate(KeyAction client, boolean allPressed, float dt) {
+				if (allPressed) {
+					log.debug("Keys pressed: {}", client.keys);
+
+					client.holdTime = 0;
+					client.state = PRESSED;
+					return client.onPress;
+				} else
+					return null;
+			}
+		};
+		static final KeyActionState PRESSED = new KeyActionState() {
+			@Override
+			Action activate(KeyAction client, boolean allPressed, float dt) {
+				if (allPressed) {
+					client.holdTime += dt;
+
+					if (client.holdTime >= HOLD_THRESHOLD) {
+						log.debug("Keys held: {}", client.keys);
+
+						client.state = WAITING;
+						return client.onHold;
 					} else
 						return null;
+				} else {
+					log.debug("Keys released: {}", client.keys);
+
+					client.state = OPEN;
+					return client.onRelease;
 				}
-			},
-			PRESSED = new KeyActionState() {
-				@Override
-				Action activate(KeyAction client, boolean allPressed, float dt) {
-					if (allPressed) {
-						client.holdTime += dt;
-						
-						if (client.holdTime >= HOLD_THRESHOLD) {
-//							log.debug("Keys held: " + client.keys);
-							
-							client.state = WAITING;
-							return client.onHold;
-						} else
-							return null;
-					} else {
-//						log.debug("Keys released: " + client.keys);
-						
-						client.state = OPEN;
-						return client.onRelease;
-					}
-				}
-			},
-			WAITING = new KeyActionState() {
-				@Override
-				Action activate(KeyAction client, boolean allPressed, float dt) {
-					if (!allPressed) {
-//						log.debug("Keys released: " + client.keys);
-						
-						client.state = OPEN;
-						return client.onRelease;
-					} else
-						return null;
-				}
-			};
-		KeyActionState() {}
-		
+			}
+		};
+		static final KeyActionState WAITING = new KeyActionState() {
+			@Override
+			Action activate(KeyAction client, boolean allPressed, float dt) {
+				if (!allPressed) {
+					log.debug("Keys released: {}", client.keys);
+
+					client.state = OPEN;
+					return client.onRelease;
+				} else
+					return null;
+			}
+		};
+
 		abstract Action activate(KeyAction client, boolean allPressed, float dt);
 	}
 }
