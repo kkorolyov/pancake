3f8adbb5f74de2b6d8c07ba0c677e368e0bcfbd1
kkorolyov <kirillk753@gmail.com>
2017-05-25 00:19:54 -0700

    Tweak InteractiveController API

--- src/test/java/dev/kkorolyov/pancake/entity/control/InteractiveEntityControllerTestInteractive.java
+++ src/test/java/dev/kkorolyov/pancake/entity/control/InteractiveEntityControllerTestInteractive.java
@@ -27,7 +27,7 @@ public class InteractiveEntityControllerTestInteractive extends Application {
 	private static final Scene scene = new Scene(new Group(canvas));
 	private static final Renderer renderer = new Renderer(canvas);
 	private static final Entity entity = new Entity(new RectangleBounds(0, 0, 1, 1), new Body(1), new Sprite(new Image("16x16.png")), buildController());
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -37,7 +37,7 @@ public class InteractiveEntityControllerTestInteractive extends Application {
 		primaryStage.setTitle(InteractiveEntityControllerTestInteractive.class.getSimpleName());
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 		primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
@@ -50,42 +50,42 @@ public class InteractiveEntityControllerTestInteractive extends Application {
 				canvas.setHeight(canvas.getHeight() + newValue.doubleValue() - oldValue.doubleValue());
 			}
 		});
-		
+
 		new AnimationTimer() {
 			private long last;
-			
+
 			public void handle(long now) {
 				if (last == 0)
 					last = now;
 				float dt = ((float) (now - last)) / 1000000000;
-				
+
 				entity.update(dt);
 				renderer.render(Arrays.asList(entity));
 				drawInfo(dt);
-				
+
 				last = now;
 			}
 		}.start();
 	}
 	private void drawInfo(float dt) {
 		GraphicsContext g = canvas.getGraphicsContext2D();
-		
+
 		g.strokeText("FPS: " + Math.round(1 / dt), 0, 10);
 		g.strokeText((entity.getBounds().getOrigin() + System.lineSeparator() + entity.getBody().toStringMultiline()), 0, canvas.getHeight() - 70);
 	}
-	
+
 	private static EntityController buildController() {
 		InteractiveEntityController controller = new InteractiveEntityController(scene);
-		
-		controller.addAction(new KeyAction(e -> e.getBody().addForce(0, -10), null, e -> e.getBody().addForce(0, 10), KeyCode.W));
-		controller.addAction(new KeyAction(e -> e.getBody().addForce(-10, 0), null, e -> e.getBody().addForce(10, 0), KeyCode.A));
-		controller.addAction(new KeyAction(e -> e.getBody().addForce(0, 10), null, e -> e.getBody().addForce(0, -10), KeyCode.S));
-		controller.addAction(new KeyAction(e -> e.getBody().addForce(10, 0), null, e -> e.getBody().addForce(-10, 0), KeyCode.D));
-		controller.addAction(new KeyAction(e -> e.getBody().setMaxSpeed(10), null, e -> e.getBody().setMaxSpeed(5), KeyCode.SHIFT));
-		controller.addAction(new KeyAction(e -> e.getBounds().getOrigin().set(0, 0), null, null, KeyCode.ESCAPE));
-		
-		controller.addAction(new KeyAction(e -> e.getBody().setMaxSpeed(1), null, e -> e.getBody().setMaxSpeed(5), MouseButton.PRIMARY));
-		controller.addAction(new KeyAction(e -> e.getBody().setMaxSpeed(-100), null, e -> e.getBody().setMaxSpeed(5), MouseButton.SECONDARY));
+
+		controller.setActions(new KeyAction(e -> e.getBody().addForce(0, -10), null, e -> e.getBody().addForce(0, 10), KeyCode.W),
+													new KeyAction(e -> e.getBody().addForce(-10, 0), null, e -> e.getBody().addForce(10, 0), KeyCode.A),
+													new KeyAction(e -> e.getBody().addForce(0, 10), null, e -> e.getBody().addForce(0, -10), KeyCode.S),
+													new KeyAction(e -> e.getBody().addForce(10, 0), null, e -> e.getBody().addForce(-10, 0), KeyCode.D),
+													new KeyAction(e -> e.getBody().setMaxSpeed(10), null, e -> e.getBody().setMaxSpeed(5), KeyCode.SHIFT),
+													new KeyAction(e -> e.getBounds().getOrigin().set(0, 0), null, null, KeyCode.ESCAPE));
+
+		controller.setActions(new KeyAction(e -> e.getBody().setMaxSpeed(1), null, e -> e.getBody().setMaxSpeed(5), MouseButton.PRIMARY),
+													new KeyAction(e -> e.getBody().setMaxSpeed(-100), null, e -> e.getBody().setMaxSpeed(5), MouseButton.SECONDARY));
 		return controller;
 	}
 }
