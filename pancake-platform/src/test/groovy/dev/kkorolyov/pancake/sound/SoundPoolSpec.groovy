46290fb1b7294585c46bd9c41a66756a80bef8cf
kkorolyov <kirillk753@gmail.com>
2017-07-30 17:37:01 -0700

    Pancake-14: Add some dinky test

---
+++ pancake-platform/src/test/groovy/dev/kkorolyov/pancake/sound/SoundPoolSpec.groovy
@@ -0,0 +1,18 @@
+package dev.kkorolyov.pancake.sound
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class SoundPoolSpec extends Specification {
+	@Shared int cacheSize = 20
+
+	SoundPool soundPool = new SoundPool(cacheSize)
+
+	def "excepts on retrieving by unassociated name"() {
+		when:
+		soundPool.get("notAName")
+
+		then:
+		thrown(NoSuchElementException)
+	}
+}
