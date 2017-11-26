274958ca31d6e2227711bbc86a00323baa26b1ff
kkorolyov <kirillk753@gmail.com>
2017-11-26 12:45:45 -0800

    Port Skillet to Kotlin: Display BigDecimal instead of Number

--- skillet/src/main/kotlin/dev/kkorolyov/pancake/skillet/ui/attribute/NumberDisplayer.kt
+++ skillet/src/main/kotlin/dev/kkorolyov/pancake/skillet/ui/attribute/NumberDisplayer.kt
@@ -6,22 +6,23 @@ import dev.kkorolyov.pancake.skillet.decorator.patterns
 import dev.kkorolyov.pancake.skillet.decorator.press
 import javafx.scene.Node
 import javafx.scene.control.Spinner
+import java.math.BigDecimal
 import java.util.regex.Pattern
 import kotlin.collections.MutableMap.MutableEntry
 
 /**
  * Displays numerical values.
  */
-object NumberDisplayer : Displayer<Number>(Number::class) {
+object NumberDisplayer : Displayer<BigDecimal>(BigDecimal::class) {
 	val NUMBER_PATTERN: Pattern = Pattern.compile(NumberStringSerializer().pattern())
 	val SEMI_NUMBER_PATTERN: Pattern = Pattern.compile(NUMBER_PATTERN.pattern().replace("d+", "d*"))
 
-	override fun display(attribute: MutableEntry<String, Number>): Node =
+	override fun display(attribute: MutableEntry<String, BigDecimal>): Node =
 			simpleDisplay(attribute.key,
 					Spinner<Double>(-Float.MAX_VALUE.toDouble(), Float.MAX_VALUE.toDouble(), attribute.value.toDouble(), .1)
 							.patterns(SEMI_NUMBER_PATTERN, NUMBER_PATTERN)
 							.press(10, 1)
 							.change(Spinner<Double>::valueProperty, { _, _, newValue ->
-								attribute.setValue(newValue)
+								attribute.setValue(BigDecimal(newValue))
 							}))
 }
