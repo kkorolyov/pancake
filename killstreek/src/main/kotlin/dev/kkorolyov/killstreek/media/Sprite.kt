a70542e808e4b0694188e5bd950c577ca216daaf
kkorolyov <kirillk753@gmail.com>
2019-06-09 15:58:15 -0700

    #32: Fix sprite migration

--- killstreek/src/main/kotlin/dev/kkorolyov/killstreek/media/Sprite.kt
+++ killstreek/src/main/kotlin/dev/kkorolyov/killstreek/media/Sprite.kt
@@ -13,24 +13,21 @@ class Sprite(
 		/** sprite sheet */
 		private val image: CompositeImage,
 		/** angle vector used for padding sprite orientation calculation */
-		orientationOffset: Vector,
+		private val orientationOffset: Vector,
 		/** number of frames in {@code image} along x-axis */
 		xFrames: Int = 1,
 		/** number of frames in {@code image} along y-axis */
 		yFrames: Int = 1,
 		/** {@code ns} between frame changes */
-		frameInterval: Long = 0
+		private var frameInterval: Long = 0
 ) : Renderable, Animated {
 	private val origin = Vector()
 	private val frames = Vector(xFrames.toFloat(), yFrames.toFloat())
 	private val frameSize = Vector(image.size.x / frames.x, image.size.y / frames.y)
 
-	private var _frameInterval: Long = frameInterval
 	private var currentFrameTime: Long = 0
-	private var _frame: Int = 0
-	private var _active: Boolean = true
-
-	private val _orientationOffset = orientationOffset
+	private var frame: Int = 0
+	private var isActive: Boolean = true
 
 	override fun render(g: GraphicsContext, position: Vector) {
 		for (layer in image) {
@@ -42,7 +39,7 @@ class Sprite(
 	}
 
 	override fun size(): Vector = frameSize
-	override fun getOrientationOffset(): Vector = _orientationOffset
+	override fun getOrientationOffset(): Vector = orientationOffset
 
 	override fun tick(dt: Long) {
 		if (!isActive) return
@@ -50,7 +47,7 @@ class Sprite(
 		currentFrameTime += dt
 		if (currentFrameTime < Math.abs(frameInterval)) return
 
-		frame += Math.round(currentFrameTime.toDouble() / frameInterval.toDouble()).toInt()  // Reversed if negative
+		frame += Math.round((currentFrameTime / frameInterval).toFloat())  // Reversed if negative
 		currentFrameTime = 0
 	}
 
@@ -58,24 +55,24 @@ class Sprite(
 		isActive = !isActive
 	}
 
-	override fun isActive(): Boolean = _active && frameInterval != 0L
+	override fun isActive(): Boolean = isActive && frameInterval != 0L
 	override fun setActive(active: Boolean) {
-		_active = active
+		isActive = active
 	}
 
-	override fun getFrame(): Int = _frame
+	override fun getFrame(): Int = frame
 	override fun setFrame(frame: Int) {
-		_frame = Math.floorMod(frame, length())
+		this.frame = Math.floorMod(frame, length())
 
 		origin.set(
-				(_frame % frames.x).toInt() * frameSize.x,
-				(_frame / frames.x) * frameSize.y
+				(frame % frames.x).toInt() * frameSize.x,
+				(frame / frames.x) * frameSize.y
 		)
 	}
 
-	override fun getFrameInterval(): Long = _frameInterval
+	override fun getFrameInterval(): Long = frameInterval
 	override fun setFrameInterval(frameInterval: Long) {
-		_frameInterval = frameInterval
+		this.frameInterval = frameInterval
 	}
 
 	override fun length(): Int = (frames.x * frames.y).toInt()
