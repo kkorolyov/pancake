3cafa9ade1020d6de92ab03dcefed1b05b488439
kkorolyov <kirillk753@gmail.com>
2019-09-09 08:08:31 -0700

    #52: Tweak registry, add loader

---
+++ pancake-platform/src/main/java/dev/kkorolyov/pancake/platform/registry/RegistryLoader.java
@@ -0,0 +1,46 @@
+package dev.kkorolyov.pancake.platform.registry;
+
+import dev.kkorolyov.pancake.platform.Config;
+import dev.kkorolyov.simplefuncs.convert.Converter;
+import dev.kkorolyov.simpleprops.Properties;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * Loads resources into a {@link Registry}.
+ * @param <K> resource key type
+ * @param <V> resource type
+ */
+@FunctionalInterface
+public interface RegistryLoader<K, V> {
+	/**
+	 * Generates a registry loader for resources defined as properties.
+	 * @param props properties to load fromm
+	 * @param resourceReader generates resource reader, invoked with loaded registry
+	 * @param <V> resource type
+	 * @return registry loader for resources defined as properties
+	 */
+	static <V> RegistryLoader<String, V> fromProperties(
+			Properties props,
+			Function<? super Registry<? super String, ? super V>, ? extends Converter<? super String, Optional<? extends V>>> resourceReader
+	) {
+		return registry -> {
+			Converter<? super String, Optional<? extends V>> reader = resourceReader.apply(registry);
+			for (Map.Entry<String, String> prop : props) {
+				reader.convert(prop.getValue())
+						.ifPresentOrElse(
+								resource -> registry.put(prop.getKey(), resource),
+								() -> Config.getLogger(RegistryLoader.class).warning("Failed to parse resource: {}", prop.getValue())
+						);
+			}
+		};
+	}
+
+	/**
+	 * Applies loaded resources to a given registry.
+	 * @param registry registry to load
+	 */
+	void load(Registry<? super K, ? super V> registry);
+}
