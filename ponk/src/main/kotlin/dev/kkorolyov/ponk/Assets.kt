40a1256d9f398b0ee98174dd42f08f0c2c21d841
kkorolyov <kirillk753@gmail.com>
2021-04-11 16:52:42 -0700

    Propagate Vector replacement for ponk

--- ponk/src/main/kotlin/dev/kkorolyov/ponk/Assets.kt
+++ ponk/src/main/kotlin/dev/kkorolyov/ponk/Assets.kt
@@ -1,45 +1,125 @@
 package dev.kkorolyov.ponk
 
+import dev.kkorolyov.pancake.core.component.ActionQueue
 import dev.kkorolyov.pancake.core.component.Bounds
+import dev.kkorolyov.pancake.core.component.Input
 import dev.kkorolyov.pancake.core.component.Transform
 import dev.kkorolyov.pancake.core.component.media.Graphic
+import dev.kkorolyov.pancake.core.component.movement.Damping
+import dev.kkorolyov.pancake.core.component.movement.Force
+import dev.kkorolyov.pancake.core.component.movement.Mass
+import dev.kkorolyov.pancake.core.component.movement.Velocity
+import dev.kkorolyov.pancake.core.component.movement.VelocityCap
+import dev.kkorolyov.pancake.core.input.HandlerReader
 import dev.kkorolyov.pancake.platform.Resources
+import dev.kkorolyov.pancake.platform.action.Action
 import dev.kkorolyov.pancake.platform.entity.EntityPool
 import dev.kkorolyov.pancake.platform.event.EventLoop
-import dev.kkorolyov.pancake.platform.math.Vector
+import dev.kkorolyov.pancake.platform.math.Vector3
+import dev.kkorolyov.pancake.platform.math.Vectors
 import dev.kkorolyov.pancake.platform.media.graphic.Renderable
 import dev.kkorolyov.pancake.platform.media.graphic.shape.Shape.Color
 import dev.kkorolyov.pancake.platform.registry.DeferredConverterFactory
 import dev.kkorolyov.pancake.platform.registry.Registry
 import dev.kkorolyov.pancake.platform.registry.ResourceReader
 
-private fun <T, F : DeferredConverterFactory<T>> load(
-	path: String,
-	strat: Class<F>
-): Registry<String, T> =
-	Resources.inStream(path).orElse(null).use {
-		Registry<String, T>().apply {
-			load(ResourceReader(DeferredConverterFactory.get(strat)).fromYaml(it))
+val actions: Registry<String, Action> =
+	Resources.inStream("actions.yaml").orElse(null).use {
+		Registry<String, Action>().apply {
+			load(
+				ResourceReader(DeferredConverterFactory.get(DeferredConverterFactory.ActionStrat::class.java)).fromYaml(
+					it
+				)
+			)
 		}
 	}
 
-val events: EventLoop.Broadcasting =
-	EventLoop.Broadcasting()
+val events: EventLoop.Broadcasting = EventLoop.Broadcasting()
 
-private val paddle: Renderable = Resources.RENDER_MEDIUM.box.apply {
+val velocityCap = Vectors.create(20.0, 20.0, 20.0)
+val damping = Vectors.create(0.0, 0.0, 0.0)
+val paddleMass = 0.01
+val ballMass = 0.001
+
+val paddleSize: Vector3 = Vectors.create(1.0, 4.0, 0.0)
+val ballSize: Vector3 = Vectors.create(1.0, 1.0, 0.0)
+
+val paddleSprite: Renderable = Resources.RENDER_MEDIUM.box.apply {
 	fill = Color.BLACK
-	size.set(1.0, 8.0)
+	size.set(paddleSize)
 }
-private val paddleBounds: Vector = Vector(1.0, 8.0)
-val entities: EntityPool = EntityPool(events).apply {
-	val player = create().apply {
-		add(Graphic(paddle))
-		add(Bounds(paddleBounds))
-		add(Transform(Vector(.5, 4.0)))
+
+val ballSprite: Renderable = Resources.RENDER_MEDIUM.box.apply {
+	fill = Color.GRAY
+	size.set(ballSize)
+}
+
+val entities: EntityPool = EntityPool(events)
+
+val player = entities.create().apply {
+	add(
+		Graphic(paddleSprite),
+		Bounds(paddleSize),
+		Transform(Vectors.create(-4.0, 0.0, 0.0)),
+		Velocity(Vectors.create(0.0, 0.0, 0.0)),
+		VelocityCap(velocityCap),
+		Damping(damping),
+		Force(Vectors.create(0.0, 0.0, 0.0)),
+		Mass(paddleMass),
+		ActionQueue()
+	)
+	Resources.inStream("input.yaml").orElse(null).use {
+		add(Input(false, HandlerReader(actions).fromYaml(it)))
 	}
-	val opponent = create().apply {
-		add(Graphic(paddle))
-		add(Bounds(paddleBounds))
-		add(Transform(Vector(3.5, 4.0)))
-	}
+}
+
+val opponent = entities.create().apply {
+	add(
+		Graphic(paddleSprite),
+		Bounds(paddleSize),
+		Transform(Vectors.create(4.0, 0.0, 0.0)),
+		Velocity(Vectors.create(0.0, 0.0, 0.0)),
+		VelocityCap(velocityCap),
+		Damping(damping),
+		Force(Vectors.create(0.0, 0.0, 0.0)),
+		Mass(paddleMass)
+	)
+}
+
+val ball = entities.create().apply {
+	add(
+		Graphic(ballSprite),
+		Bounds(ballSize),
+		Transform(Vectors.create(0.0, 0.0, 0.0)),
+		Velocity(Vectors.create(-10.0, 0.0, 0.0)),
+		VelocityCap(Vectors.create(10.0, 10.0, 10.0)),
+		Force(Vectors.create(0.0, 0.0, 0.0)),
+		Mass(ballMass)
+	)
+}
+
+val goalPlayer = entities.create().apply {
+	add(
+		Bounds(Vectors.create(10.0, 10.0, 0.0)),
+		Transform(Vectors.create(-15.0, 0.0, 0.0))
+	)
+}
+val goalOpponent = entities.create().apply {
+	add(
+		Bounds(Vectors.create(10.0, 10.0, 0.0)),
+		Transform(Vectors.create(15.0, 0.0, 0.0))
+	)
+}
+
+val top = entities.create().apply {
+	add(
+		Bounds(Vectors.create(10.0, 10.0, 0.0)),
+		Transform(Vectors.create(0.0, 15.0, 0.0))
+	)
+}
+val bottom = entities.create().apply {
+	add(
+		Bounds(Vectors.create(10.0, 10.0, 0.0)),
+		Transform(Vectors.create(0.0, -15.0, 0.0))
+	)
 }
