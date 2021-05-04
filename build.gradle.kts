0a3d3cfd2e8994f5ed40e1f77a801b309e202144
kkorolyov <kirillk753@gmail.com>
2021-05-04 09:22:40 -0700

    Re-add docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,28 @@ tasks.register("allDeps") {
 
 	dependsOn(subprojects.map { it.tasks.withType<DependencyReportTask>() })
 }
+
+tasks.register("allDocs") {
+	group = "documentation"
+	description = "Bundles all project documentation together"
+
+	val destination = "${project.buildDir}/docs"
+	val subDocs = subprojects.map {
+		it.tasks.withType<Javadoc>() + it.tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>()
+			.filter { it.name == "dokkaHtml" }
+	}.flatten()
+
+	dependsOn(subDocs)
+	doLast {
+		copy {
+			from("docs")
+			into(destination)
+		}
+		subDocs.forEach {
+			copy {
+				from(it)
+				into("$destination/${it.project.name}")
+			}
+		}
+	}
+}
