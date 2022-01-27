92da29044d16f811065a456bdc1f627f8a5a97bb
kkorolyov <kirillk753@gmail.com>
2022-01-26 21:27:46 -0800

    #87: Add expanded entity details

--- editor/src/main/kotlin/dev/kkorolyov/pancake/editor/view/EntityViews.kt
+++ editor/src/main/kotlin/dev/kkorolyov/pancake/editor/view/EntityViews.kt
@@ -1,11 +1,12 @@
 package dev.kkorolyov.pancake.editor.view
 
-import dev.kkorolyov.pancake.editor.controller.DataSelection
 import dev.kkorolyov.pancake.editor.controller.DataPoller
+import dev.kkorolyov.pancake.editor.controller.DataSelection
 import dev.kkorolyov.pancake.editor.data.EntityData
 import dev.kkorolyov.pancake.editor.getComponentDetails
 import javafx.collections.FXCollections
 import tornadofx.View
+import tornadofx.bind
 import tornadofx.column
 import tornadofx.field
 import tornadofx.fieldset
@@ -13,7 +14,9 @@ import tornadofx.form
 import tornadofx.integerBinding
 import tornadofx.label
 import tornadofx.onChange
+import tornadofx.onDoubleClick
 import tornadofx.onSelectionChange
+import tornadofx.scrollpane
 import tornadofx.select
 import tornadofx.tab
 import tornadofx.tableview
@@ -23,12 +26,14 @@ import java.util.Comparator.comparing
 class EntitiesTable : View() {
 	private val poller: DataPoller by inject()
 	private val dataSelection: DataSelection by inject()
+	private val expandedEntityDetails: ExpandedEntityDetails by inject()
 
 	override val root = tableview(poller.entities) {
 		column<EntityData, Int>("ID") { it.value.id }
 		column<EntityData, Number>("Components") { integerBinding(it.value.components) { size } }
 
 		onSelectionChange(dataSelection.entityData::bind)
+		onDoubleClick { expandedEntityDetails.openWindow() }
 	}
 }
 
@@ -54,5 +59,26 @@ class EntityDetails : View() {
 				}
 			}
 		}
+	}
+}
+
+class ExpandedEntityDetails : View() {
+	private val dataSelection: DataSelection by inject()
+
+	override val root = scrollpane {
+		form {
+			fieldset {
+				children.bind(dataSelection.entityData.components) {
+					field(it.name.value) {
+						add(getComponentDetails(it))
+					}
+				}
+			}
+		}
+	}
+
+	override fun onDock() {
+		title = dataSelection.entityData.id.value.toString()
+		dataSelection.entityData.id.onChange { title = it.toString() }
 	}
 }
