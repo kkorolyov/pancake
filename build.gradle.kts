6d2ce36ac1b3a2a20426565bb554b74e4ce031a9
kkorolyov <kirillk753@gmail.com>
2022-06-15 01:39:42 -0700

    #112: Generate docs into /docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
@@ -33,23 +35,82 @@ tasks.register("allDocs") {
 	group = "documentation"
 	description = "Bundles all project documentation together"
 
-	val destination = "${project.buildDir}/docs"
+	val destination = "$projectDir/docs"
 	val subDocs = subprojects.map {
 		it.tasks.withType<DokkaTask>().filter { it.name == "dokkaHtml" }.ifEmpty { it.tasks.withType<Javadoc>() }
 	}.flatten()
 
 	dependsOn(subDocs)
 	doLast {
-		copy {
-			from("docs")
-			into(destination)
-		}
 		subDocs.forEach {
 			copy {
 				from(it)
 				into("$destination/${it.project.name}")
 			}
 		}
+		// generate index
+		File("$destination/index.html").writeText(
+			"""
+			<!DOCTYPE HTML>
+			<html meta lang="en">
+			
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			
+				<title>Pancake Javadoc</title>
+			
+				<link rel="shortcut icon" type="image/png" href="pancake.png">
+			
+				<style>
+					table {
+						font-size: 2em;
+					}
+			
+					table>caption {
+						display: flex;
+						align-items: center;
+					}
+			
+					td {
+						background-color: #d38d5f;
+			
+						transition: background-color .5s;
+					}
+					td:hover {
+						background-color: #e9c6af;
+					}
+			
+					a {
+						display: block;
+						padding: 16px;
+			
+						text-decoration: none;
+						color: inherit;
+					}
+				</style>
+			</head>
+			
+			<body>
+				<table>
+					<caption>
+						<img src="pancake.png" alt="pancake"> Modules
+					</caption>
+					<tr>
+						${File(destination).listFiles()?.filter { it.isDirectory }?.map { it.name }?.joinToString("") { "<td><a href=\"$it/index.html\">$it</a></td>" } ?: ""}
+					</tr>
+				</table>
+			</body>
+			
+			</html>
+			""".trimIndent()
+		)
+	}
+}
+
+tasks.clean {
+	doFirst {
+		File("$projectDir/docs").listFiles { _, name -> name != "pancake.png" }?.let { delete(*it) }
 	}
 }
 
