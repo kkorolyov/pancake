8c1a4f38cea08d7ccd496dae4331e36f11ead8c7
kkorolyov <kirillk753@gmail.com>
2022-07-19 23:11:33 -0700

    #123: Remove JFX

--- graphics-jfx/src/main/kotlin/dev/kkorolyov/pancake/graphics/jfx/component/Graphic.kt
+++
@@ -1,9 +0,0 @@
-package dev.kkorolyov.pancake.graphics.jfx.component
-
-import dev.kkorolyov.pancake.graphics.jfx.drawable.Drawable
-import dev.kkorolyov.pancake.platform.entity.Component
-
-/**
- * A [Drawable] component.
- */
-class Graphic(private val delegate: Drawable) : Component, Drawable by delegate
