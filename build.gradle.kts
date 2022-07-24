b23d6a9161bae08165a75ee13b246f6e0015837c
kkorolyov <kirillk753@gmail.com>
2022-07-24 13:42:18 -0700

    #103: Bump dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	java
 	id("org.ajoberstar.reckon") version "0.+"
 	kotlin("jvm") version "1.7.+" apply false
-	id("org.jetbrains.dokka") version "1.6.+" apply false
+	id("org.jetbrains.dokka") version "1.7.+" apply false
 }
 
 tasks.wrapper {
