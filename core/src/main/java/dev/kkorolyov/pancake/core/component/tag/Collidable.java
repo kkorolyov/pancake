cad91e71d4dfedd1a3d47f266cb49a27eb0c48e2
kkorolyov <kirillk753@gmail.com>
2022-08-16 15:07:36 -0700

    #144: Split correctable + collidable to tag components

---
+++ core/src/main/java/dev/kkorolyov/pancake/core/component/tag/Collidable.java
@@ -0,0 +1,8 @@
+package dev.kkorolyov.pancake.core.component.tag;
+
+import dev.kkorolyov.pancake.platform.entity.Component;
+
+/**
+ * A marker indicating that the owning entity can respond to collisions.
+ */
+public final class Collidable implements Component {}
---
+++ core/src/main/java/dev/kkorolyov/pancake/core/component/tag/Correctable.java
@@ -0,0 +1,8 @@
+package dev.kkorolyov.pancake.core.component.tag;
+
+import dev.kkorolyov.pancake.platform.entity.Component;
+
+/**
+ * A marker indicating that the owning entity can respond to boundary intersections.
+ */
+public final class Correctable implements Component {}
