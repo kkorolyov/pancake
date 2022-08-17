ec45ac027b30b760336b5e698b0cafc7117e47b8
kkorolyov <kirillk753@gmail.com>
2022-08-16 21:18:52 -0700

    #144: Make Model mutable

--- graphics/src/main/kotlin/dev/kkorolyov/pancake/graphics/component/Model.kt
+++ graphics/src/main/kotlin/dev/kkorolyov/pancake/graphics/component/Model.kt
@@ -7,4 +7,20 @@ import dev.kkorolyov.pancake.platform.entity.Component
 /**
  * Combines a set of meshes with the program to draw them.
  */
-class Model(val program: Program, vararg val meshes: Mesh) : Component
+class Model(var program: Program, vararg meshes: Mesh) : Component {
+	private val _meshes = mutableListOf(*meshes)
+
+	/**
+	 * Ordered model meshes.
+	 */
+	val meshes: List<Mesh>
+		get() = _meshes
+
+	/**
+	 * Sets current meshes to [meshes].
+	 */
+	fun setMeshes(vararg meshes: Mesh) {
+		_meshes.clear()
+		_meshes.addAll(meshes)
+	}
+}
