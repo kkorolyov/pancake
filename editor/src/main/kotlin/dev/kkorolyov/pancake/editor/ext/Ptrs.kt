64a94be4427d1601f6c998ad2f8478fd871d4e1f
kkorolyov <kirillk753@gmail.com>
2022-12-31 13:02:13 -0800

    #154: Add more ptrs

--- editor/src/main/kotlin/dev/kkorolyov/pancake/editor/ext/Ptrs.kt
+++ editor/src/main/kotlin/dev/kkorolyov/pancake/editor/ext/Ptrs.kt
@@ -2,12 +2,17 @@ package dev.kkorolyov.pancake.editor.ext
 
 import imgui.type.ImBoolean
 import imgui.type.ImDouble
+import imgui.type.ImInt
 
 /**
- * Returns a pointer to [this] value.
+ * Returns an `imgui` pointer initialized to [this] value.
  */
 fun Boolean.ptr(): ImBoolean = ImBoolean(this)
 /**
- * Returns a pointer to [this] value.
+ * Returns an `imgui` pointer initialized to [this] value.
+ */
+fun Int.ptr(): ImInt = ImInt(this)
+/**
+ * Returns an `imgui` pointer initialized to [this] value.
  */
 fun Double.ptr(): ImDouble = ImDouble(this)
