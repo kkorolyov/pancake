6659027b9ebbbadf84b05c5316893f6b9f24c147
kkorolyov <kirillk753@gmail.com>
2023-03-12 15:06:00 -0700

    support Window constraints

--- editor/src/main/kotlin/dev/kkorolyov/pancake/editor/widget/Window.kt
+++ editor/src/main/kotlin/dev/kkorolyov/pancake/editor/widget/Window.kt
@@ -2,6 +2,7 @@ package dev.kkorolyov.pancake.editor.widget
 
 import dev.kkorolyov.pancake.editor.Widget
 import dev.kkorolyov.pancake.editor.ext.ptr
+import dev.kkorolyov.pancake.platform.math.Vector2
 import imgui.ImGui
 import imgui.type.ImBoolean
 
@@ -16,6 +17,8 @@ class Window(
 	var label: String,
 	private val content: Widget,
 	private val visiblePtr: ImBoolean = true.ptr(),
+	private val minSize: Vector2 = Vector2.of(0.0, 0.0),
+	private val maxSize: Vector2 = Vector2.of(Double.MAX_VALUE, Double.MAX_VALUE),
 	private val flags: Int = 0,
 	private val setup: () -> Unit = {}
 ) : Widget {
@@ -36,8 +39,9 @@ class Window(
 
 	override fun invoke() {
 		if (visible) {
+			ImGui.setNextWindowSizeConstraints(minSize.x.toFloat(), minSize.y.toFloat(), maxSize.x.toFloat(), maxSize.y.toFloat())
 			setup()
-			if (ImGui.begin(label, visiblePtr, flags)) {
+			if (ImGui.begin(label, visiblePtr, flags) || ImGui.isWindowDocked()) {
 				content()
 			}
 			ImGui.end()
