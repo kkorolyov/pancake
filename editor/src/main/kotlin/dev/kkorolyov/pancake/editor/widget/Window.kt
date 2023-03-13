9082ceb2b8e886d312398d938c894ebcd351000f
kkorolyov <kirillk753@gmail.com>
2023-03-12 23:27:33 -0700

    support fullscreen windows more simply

--- editor/src/main/kotlin/dev/kkorolyov/pancake/editor/widget/Window.kt
+++ editor/src/main/kotlin/dev/kkorolyov/pancake/editor/widget/Window.kt
@@ -4,7 +4,15 @@ import dev.kkorolyov.pancake.editor.Widget
 import dev.kkorolyov.pancake.editor.ext.ptr
 import dev.kkorolyov.pancake.platform.math.Vector2
 import imgui.ImGui
+import imgui.flag.ImGuiWindowFlags
 import imgui.type.ImBoolean
+
+private const val FULLSCREEN_FLAGS = ImGuiWindowFlags.NoDecoration or
+		ImGuiWindowFlags.NoBackground or
+		ImGuiWindowFlags.NoMove or
+		ImGuiWindowFlags.NoBringToFrontOnFocus or
+		ImGuiWindowFlags.NoFocusOnAppearing or
+		ImGuiWindowFlags.NoNavFocus
 
 /**
  * Renders [content] within a top-level window.
@@ -19,8 +27,8 @@ class Window(
 	private val visiblePtr: ImBoolean = true.ptr(),
 	private val minSize: Vector2 = Vector2.of(0.0, 0.0),
 	private val maxSize: Vector2 = Vector2.of(Double.MAX_VALUE, Double.MAX_VALUE),
-	private val flags: Int = 0,
-	private val setup: () -> Unit = {}
+	private var flags: Int = 0,
+	private val fullscreen: Boolean = false
 ) : Widget {
 	/**
 	 * Whether this is visible.
@@ -28,6 +36,10 @@ class Window(
 	var visible: Boolean
 		get() = visiblePtr.get()
 		set(value) = visiblePtr.set(value)
+
+	init {
+		if (fullscreen) flags = flags or FULLSCREEN_FLAGS
+	}
 
 	/**
 	 * Renders this as the focused window.
@@ -40,7 +52,12 @@ class Window(
 	override fun invoke() {
 		if (visible) {
 			ImGui.setNextWindowSizeConstraints(minSize.x.toFloat(), minSize.y.toFloat(), maxSize.x.toFloat(), maxSize.y.toFloat())
-			setup()
+			if (fullscreen) {
+				ImGui.getMainViewport().let { ImGui.setNextWindowPos(it.posX, it.posY) }
+				ImGui.getIO().displaySize.let { ImGui.setNextWindowSize(it.x, it.y) }
+			}
+			// ensure shown in cases of child/parent windows docked to it - to avoid visibility feedback loop
+			// FIXME this doesn't handle cases of grand-child/parent windows docked to this window
 			if (ImGui.begin(label, visiblePtr, flags) || ImGui.isWindowDocked()) {
 				content()
 			}
