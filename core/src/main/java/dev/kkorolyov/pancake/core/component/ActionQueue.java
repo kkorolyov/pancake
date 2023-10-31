af0d8096b4a9a846416ab4e21bd17a274e039a69
kkorolyov <kirillk753@gmail.com>
2023-10-31 01:27:14 -0700

    support queueing from actions

--- core/src/main/java/dev/kkorolyov/pancake/core/component/ActionQueue.java
+++ core/src/main/java/dev/kkorolyov/pancake/core/component/ActionQueue.java
@@ -3,33 +3,55 @@ package dev.kkorolyov.pancake.core.component;
 import dev.kkorolyov.pancake.platform.action.Action;
 import dev.kkorolyov.pancake.platform.entity.Component;
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.Queue;
 
 /**
  * A collection of queued actions to apply to containing entities.
  * The same instance can be shared between multiple entities, which will all get the same actions applied to them.
  */
 public final class ActionQueue implements Component, Iterable<Action> {
-	private final Queue<Action> actions = new ArrayDeque<>();
+	private final Collection<Action> actions = new ArrayList<>();
+	private final Collection<Action> buffer = new ArrayList<>();
+	private boolean locked;
 
 	/**
 	 * Adds {@code action} to the end of this queue.
+	 * If this is called after calling {@link #iterator}, but before calling {@link #clear()}, buffers the action instead, and only adds it at end of the next {@link #clear()}.
+	 * This is to support predictable, consistent behavior where an action in this queue queues a new action.
 	 */
 	public void add(Action action) {
-		actions.add(action);
+		(locked ? buffer : actions).add(action);
+	}
+	/**
+	 * Removes {@code action} from this queue (or from its buffer, see {@link #add(Action)}), if it exists.
+	 * Returns whether such an instance existed and was removed.
+	 */
+	public boolean remove(Action action) {
+		return (locked ? buffer : actions).remove(action);
+	}
+	/**
+	 * Returns whether this queue (or its buffer, see {@link #add(Action)}) contains {@code action}.
+	 */
+	public boolean contains(Action action) {
+		return (locked ? buffer : actions).contains(action);
 	}
 
 	/**
-	 * Clears all queued actions.
+	 * Clears all queued actions, then adds any buffered actions.
 	 */
 	public void clear() {
+		locked = false;
 		actions.clear();
+
+		actions.addAll(buffer);
+		buffer.clear();
 	}
 
 	@Override
 	public Iterator<Action> iterator() {
+		locked = true;
 		return actions.iterator();
 	}
 }
