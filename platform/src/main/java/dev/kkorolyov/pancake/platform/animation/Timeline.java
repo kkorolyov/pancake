7da906d231d94fbcd0ea73be7ce991d45fbbb50c
kkorolyov <kirillk753@gmail.com>
2024-12-22 20:33:03 -0800

    add some Timeline APIs

--- platform/src/main/java/dev/kkorolyov/pancake/platform/animation/Timeline.java
+++ platform/src/main/java/dev/kkorolyov/pancake/platform/animation/Timeline.java
@@ -9,19 +9,34 @@ import java.util.Objects;
 import java.util.TreeMap;
 
 /**
- * A collection of key {@link Frame}s at specific offsets, returning interpolated frames at any offsets between {@code 0} and {@link #size()}.
+ * A collection of keyframe {@link Frame}s at specific offsets, returning interpolated frames at any offsets between {@code 0} and {@link #size()}.
  */
 public final class Timeline<T extends Frame<T>> implements Iterable<Map.Entry<Integer, T>> {
 	private final NavigableMap<Integer, T> keyframes = new TreeMap<>();
 
 	/**
-	 * Sets the key {@code frame} at {@code offset}, replacing any previously set keyframe at that point.
+	 * Returns whether this timeline has a keyframe at {@code offset}.
+	 */
+	public boolean contains(int offset) {
+		return keyframes.containsKey(offset);
+	}
+	/**
+	 * Sets the keyframe {@code frame} at {@code offset}, replacing any previously set keyframe at that point.
 	 * {@code offset} must be {@code >= 0}.
 	 */
 	public void put(int offset, T frame) {
 		ArgVerify.greaterThanEqual("offset", 0, offset);
 
 		keyframes.put(offset, frame);
+	}
+	/**
+	 * Removes the keyframe at {@code offset} and returns it, if any.
+	 * Throws {@link IllegalArgumentException} if {@code offset} is less than {@code 0} or greater than {@link #size()}.
+	 */
+	public T remove(int offset) {
+		ArgVerify.betweenInclusive("offset", 0, size(), offset);
+
+		return keyframes.remove(offset);
 	}
 
 	/**
