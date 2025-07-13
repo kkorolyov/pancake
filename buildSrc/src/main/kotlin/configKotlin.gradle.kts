2f3881482873bb1126319fefe837b42e0ab4cfba
kkorolyov <kirillk753@gmail.com>
2025-07-12 23:02:02 -0700

    simplify buildfile

--- buildSrc/src/main/kotlin/configKotlin.gradle.kts
+++ buildSrc/src/main/kotlin/configKotlin.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 }
 
 tasks.compileJava {
-	val file = file("$projectDir/src/main/java/module-info.java")
+	val moduleInfoFile = file("$projectDir/src/main/java/module-info.java")
 
-	if (file.exists()) {
-		val moduleName = """(?<=module\s)[\w.]+""".toRegex().find(file("$projectDir/src/main/java/module-info.java").readText())!!.value
+	if (moduleInfoFile.exists()) {
+		val moduleName = """(?<=module\s)[\w.]+""".toRegex().find(moduleInfoFile.readText())!!.value
 		options.compilerArgs = listOf(
 			"--patch-module", "$moduleName=${sourceSets["main"].output.asPath}"
 		)
