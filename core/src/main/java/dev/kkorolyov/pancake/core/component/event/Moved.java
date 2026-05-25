41fa26b24bea4d328c1986d5e70a4a6340a62df9
kkorolyov <kirillk753@gmail.com>
2026-05-25 08:23:44 -0700

    add moved tag

---
+++ core/src/main/java/dev/kkorolyov/pancake/core/component/event/Moved.java
@@ -0,0 +1,9 @@
+package dev.kkorolyov.pancake.core.component.event;
+
+import dev.kkorolyov.pancake.platform.entity.Component;
+
+/**
+ * A marker indicating the owning entity has recently moved.
+ * Intended for spatial structure optimizations.
+ */
+public final class Moved implements Component {}
